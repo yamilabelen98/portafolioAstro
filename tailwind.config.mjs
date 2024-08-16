d2c76133f03db73c7ce59c2adc787bf925344fa6
yamilabelen98 <yami40521@gmail.com>
2024-08-16 20:24:55 -0300

    Arreglos

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,14 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	darkMode:'class',
-	theme: {
-		extend: {
-			colors: {
-				'icon-light': '#000000', // Color del ícono en modo claro
-				'icon-dark': '#ffffff',  // Color del ícono en modo oscuro
-			  },
-		},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        "icon-light": "#000000", // Color del ícono en modo claro
+        "icon-dark": "#ffffff", // Color del ícono en modo oscuro
+      },
+      animation: {
+        "background-shine": "background-shine 2s linear infinite",
+      },
+      keyframes: {
+        "background-shine": {
+          from: { backgroundPosition: "0 0" },
+          to: { backgroundPosition: "-200% 0" },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
